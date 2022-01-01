c769bfd402f6cbc350227bbe3fd30c7221c38742
ig <ilkergonenc@gmail.com>
2022-01-01 17:28:51 +0300

    implement mdg:validated-method pack

--- imports/api/tasks/tasksMethods.js
+++ imports/api/tasks/tasksMethods.js
@@ -1,55 +1,67 @@
-import { check } from 'meteor/check';
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import SimpleSchema from "simpl-schema";
 import { Tasks } from '/imports/api/tasks/tasks';
 
-Meteor.methods({
-  'tasks.insert'(text) {
-    check(text, String);
-
-    if (!this.userId) {
-      throw new Meteor.Error('Not authorized.');
+const taskInsert = new ValidatedMethod({
+  name: 'tasks.insert',
+  validate: new SimpleSchema({
+    text: { type: String }
+  }).validator(),
+  run({ text }) {
+    if (!this.userId) throw new Meteor.Error('Not authorized.');
+    try {
+      Tasks.insert({
+        text,
+        createdAt: new Date(),
+        userId: this.userId,
+      });
+    } catch (error) {
+      throw new Meteor.Error('Could not insert collection.', error); 
     }
-
-    Tasks.insert({
-      text,
-      createdAt: new Date(),
-      userId: this.userId,
-    });
-  },
-
-  'tasks.remove'(taskId) {
-    check(taskId, String);
-
-    if (!this.userId) {
-      throw new Meteor.Error('Not authorized.');
-    }
-
-    const task = Tasks.findOne({ _id: taskId, userId: this.userId });
-
-    if (!task) {
-      throw new Meteor.Error('Access denied.');
-    }
-
-    Tasks.remove(taskId);
-  },
-
-  'tasks.setIsChecked'(taskId, isChecked) {
-    check(taskId, String);
-    check(isChecked, Boolean);
-
-    if (!this.userId) {
-      throw new Meteor.Error('Not authorized.');
-    }
-
-    const task = Tasks.findOne({ _id: taskId, userId: this.userId });
-
-    if (!task) {
-      throw new Meteor.Error('Access denied.');
-    }
-
-    Tasks.update(taskId, {
-      $set: {
-        isChecked,
-      },
-    });
-  },
+  }
 });
+
+const taskRemove = new ValidatedMethod({
+  name: 'tasks.remove',
+  validate: new SimpleSchema({
+    taskId: { type: String }
+  }).validator(),
+  run({ taskId }) {
+    if (!this.userId) throw new Meteor.Error('Not authorized.');
+    const task = Tasks.findOne({ _id: taskId, userId: this.userId });
+    if (!task) throw new Meteor.Error('Access denied.');
+    try {
+      Tasks.remove(taskId);
+    } catch (error) {
+      throw new Meteor.Error('Could not remove collection.', error); 
+    }
+  }
+});
+
+const taskSetIsChecked = new ValidatedMethod({
+  name: 'tasks.setIsChecked',
+  validate: new SimpleSchema({
+    taskId: { type: String },
+    isChecked: { type: Boolean },
+  }).validator(),
+  run({ taskId, isChecked }) {
+    if (!this.userId) throw new Meteor.Error('Not authorized.');
+    const task = Tasks.findOne({ _id: taskId, userId: this.userId });
+    if (!task) throw new Meteor.Error('Access denied.');
+    try {
+      Tasks.update(taskId, {
+        $set: {
+          isChecked,
+        },
+      });
+    } catch (error) {
+      throw new Meteor.Error('Could not update collection.', error); 
+    }
+  }
+});
+
+export {
+  taskInsert,
+  taskRemove,
+  taskSetIsChecked
+};
