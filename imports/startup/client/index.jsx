0773be1ae057f02fc66281ac063c9743f7034398
ig <ilkergonenc@gmail.com>
2022-01-02 12:10:40 +0300

    fix import

--- imports/startup/client/index.jsx
+++ imports/startup/client/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { App } from '/imports/ui/App';
 
 Meteor.startup(() => {
   ReactDOM.render(
