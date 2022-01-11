79e46f8d89dd7ea9d28ee35c9f1a5c0f3521cfb6
ig <ilkergonenc@gmail.com>
2022-01-11 16:46:44 +0300

    ass tasks page with grapher query publication with in useTracker

---
+++ imports/ui/tasks/TasksQuery.jsx
@@ -0,0 +1,52 @@
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { 
+  Box, 
+  List, 
+  ListItem, 
+  Flex, 
+  Spacer 
+} from '@chakra-ui/react';
+
+import tasksQuery from '/imports/api/tasks/queries/tasks';
+
+import { TaskItem } from './TaskItem';
+
+export const TasksQuery = () => {
+
+  const { tasks, isLoading } = useTracker(() => {
+    const noDataAvailable = { tasks: [] };
+    if (!Meteor.user()) return noDataAvailable;
+    const query = tasksQuery.clone();
+    const handler = query.subscribe();
+    if (!handler.ready()) return { ...noDataAvailable, isLoading: true };
+    const tasks = query.fetch();
+    // console.log(tasks);
+    return {tasks};
+  });
+  // const todoHandler = Meteor.subscribe('taskTodo', todoId);
+  // const todos = Todos.findOne({ _id: task.todoId });
+  // task.todos = todos;
+  // const imageHandler = Meteor.subscribe('taskImages', taskId);
+  // const images = Images.find({ taskId: task._id }).fetch();
+  // task.images = images;
+  return (
+    <Box>
+      {isLoading && <div className="loading">loading...</div>}
+      <List spacing={3}>
+        {tasks.map(task => (
+          <ListItem>
+          <Flex>
+            <Link to={`${task._id}`} >{task.text}</Link>
+            <Spacer />
+            <p>@{task.user.username}</p>
+          </Flex>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
