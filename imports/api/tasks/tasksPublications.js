fa2cffec99fefd9fc5a92408f08aa57f77d004ac
ig <ilkergonenc@gmail.com>
2022-01-11 14:55:53 +0300

    add task publish

--- imports/api/tasks/tasksPublications.js
+++ imports/api/tasks/tasksPublications.js
@@ -4,3 +4,7 @@ import TasksCollection from './TasksCollection';
 Meteor.publish('tasks', function publishTasks() {
   return TasksCollection.find({ userId: this.userId }, { fields: TasksCollection.publicFields });
 });
+
+Meteor.publish('task', function publishTask(taskId) {
+  return TasksCollection.find({ _id: taskId }, { fields: TasksCollection.publicFields });
+});
