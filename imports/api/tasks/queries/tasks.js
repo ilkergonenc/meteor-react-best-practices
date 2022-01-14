1f1b7d8456116b9bae16c985d38636998d3a55c9
ig <ilkergonenc@gmail.com>
2022-01-15 02:49:54 +0300

    fix sort key

--- imports/api/tasks/queries/tasks.js
+++ imports/api/tasks/queries/tasks.js
@@ -7,10 +7,9 @@ export default createQuery({
       if(params.userId)     filters.userId = params.userId
       if(params.isChecked)  filters.isChecked = params.isChecked
     },
-    $options: { createdAt: -1 },
+    $options: { sort: { createdAt: -1 } },
     text: 1, 
     isChecked: 1,
-    createdAt: 1,
     user: {
       username: 1
     }
