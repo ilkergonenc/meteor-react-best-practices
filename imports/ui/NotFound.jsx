5b41c3f0b41ad8f14ae2c262f502e50b6a1e8153
ig <ilkergonenc@gmail.com>
2022-01-15 13:24:57 +0300

    edit notfound with metadata and i18n

--- imports/ui/NotFound.jsx
+++ imports/ui/NotFound.jsx
@@ -1,4 +1,17 @@
 import React from "react";
-export const NotFound = () => (
-  <p>NOT_FOUND</p>
-);
+import DocumentMeta from 'react-document-meta';
+import { Trans, useTranslation } from 'react-i18next';
+
+export const NotFound = () => {
+  const [ t ] = useTranslation('translations');
+  const meta = {
+    title: `${t('notFound.title')}`,
+    description: `${t('notFound.desc')}`,
+  };
+  return (
+    <DocumentMeta {...meta}>
+      <h2><Trans i18nKey="notFound.title"></Trans></h2>
+      <p><Trans i18nKey="notFound.desc"></Trans></p>
+    </DocumentMeta>
+  );
+};
